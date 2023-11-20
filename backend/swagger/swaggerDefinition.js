2edba1878d19ea71e01680d5c54a636fe99368f8
Rajneesh Yadav <112797112+Uselessme21@users.noreply.github.com>
2023-11-20 23:26:23 +0530

    Update swaggerDefinition.js

--- backend/swagger/swaggerDefinition.js
+++ backend/swagger/swaggerDefinition.js
@@ -7,7 +7,7 @@ const swaggerDefinition = {
     },
     servers: [
       {
-        url: 'https://bytive-assignment-stretch-frn4.vercel.app/',
+        url: 'https://bytive-assignment-stretch-frn4.vercel.app',
         description: 'API',
       },
     ],
